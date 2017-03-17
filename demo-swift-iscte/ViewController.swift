32dd06f112e5c0ec64a3158c7f33b93a97497499
Filipe Patricio <filipepatricio@Filipes-MacBook-Pro.local>
2017-03-17 13:08:00 +0000

    Add button action

--- demo-swift-iscte/ViewController.swift
+++ demo-swift-iscte/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var textField: UITextField!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func changeTitleAction(_ sender: Any) {
+    self.titleLabel.text = self.textField.text
+  }
 
 }
 
