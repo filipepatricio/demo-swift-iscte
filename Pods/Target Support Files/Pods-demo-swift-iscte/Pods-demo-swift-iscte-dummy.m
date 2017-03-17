224008e8e01d739e5c5faf92f1fe1759c0d17334
Filipe Patricio <filipepatricio@Filipes-MacBook-Pro.local>
2017-03-17 18:41:02 +0000

    Add pods

---
+++ Pods/Target Support Files/Pods-demo-swift-iscte/Pods-demo-swift-iscte-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_demo_swift_iscte : NSObject
+@end
+@implementation PodsDummy_Pods_demo_swift_iscte
+@end
---
+++ Pods/Target Support Files/Pods-demo-swift-iscte/Pods-demo-swift-iscte-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_demo_swift_iscteVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_demo_swift_iscteVersionString[];
+
